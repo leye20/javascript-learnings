aaa766ec77dd2bd21af78ea265fde2565856ebdc
leye20 <leyeagbo@gmail.com>
2020-03-06 13:44:52 -0700

    added hover text to salmon colour from gray!

--- js/day-5.js
+++ js/day-5.js
@@ -6,23 +6,25 @@
  // Single line, comment!
 
  // Basic assignment:
-
  var myVariable = '38.9587';
- console.log( myVariable + 5 ); // Oop, one of these is a string so plus is concatenating!
 
- myVariable = parseFloat( myVariable ); //Flaoting point number, decimal maintained.
+ myVariable = myVariable + 5;
+ console.log( myVariable ); // Oop, one of these is a string so plus is concatenating!
+
+ // Type-handling:
+ myVariable = parseFloat( myVariable ); // Floating point number, decimal maintained.
  console.log( myVariable ); // Is now a numeric value.
 
- myVariable = parseInt( myVariable ); // Converted to integer, decimal remived.
+ myVariable = parseInt( myVariable ); // Converted to integer, decimal removed.
  console.log( myVariable); // 38
 
- myVariable = Number( myVariable ); // Convert to any number type it can.
- console.log( myVariable ); // No Change here.
+ myVariable = Number( myVariable ); // Converted to any number type it can.
+ console.log( myVariable ); // No change here.
 
  myVariable = myVariable + 5; // Addition. Beware "+" as it can add, or concatenate, depending on if any of the operands are strings!
  console.log( myVariable );
 
- myVariable = myVariable.toString(); // Converts back to a string.
+ myVariable = myVariable.toString(); // Convert back to a string.
  console.log( myVariable );
 
  /**
@@ -30,16 +32,16 @@
   */
 
 var myArrayOfDataTypes = [
-    // Boolean
+    // Booleans:
     true,
     false,
-    // Null
+    // Null:
     null,
-    // Float
+    // Float:
     34,
-    168.783,
+    168.793,
     // String:
-    'Hello, World',
+    'Hello, World!',
     // Vaue not defined:
     undefined,
     // Not a number:
@@ -57,19 +59,45 @@ var myArrayOfDataTypes = [
 
  // Let's loop through the array so we don't have to typ out each item!
  for ( var i = 0; i < myArrayOfDataTypes.length; i++ )
- { // ( ASSIGMNMENT; TERMINATION CONDITION; ITERATION )
+ { // ( ASSIGNMENT; TERMINATION CONDITION; ITERATION )
     // Create datalist title.
     var dataListTitle = document.createElement( 'DT' );
     dataListTitle.textContent = typeof myArrayOfDataTypes[i]; // Output the datatype.
 
-    // Create datalist data
+    // Create datalist data.
     var dataListData = document.createElement( 'DD' );
     dataListData.textContent = myArrayOfDataTypes[i];
 
     // Add our new elements to our datalist.
     myDataList.appendChild( dataListTitle );
     myDataList.appendChild( dataListData );
- }
+}
 
- // Add ALL of that to the body! Yay! We should be see it now! 
+  var myHeadingText = 'Our Data-Type Test:';
+ // This is adding HTML to the end of our body.
+ document.body.innerHTML += `<!-- Back ticks are used in JS for multi-line strings. -->
+    <h2>
+        `+myHeadingText/*Concatenating strings!*/+`    
+    </h2>
+`;
+// Add ALL of that to the body! Yay! We should be see it now! 
  document.body.appendChild( myDataList );
+
+ /**
+  * Collecting elements.
+  */
+
+// Get by ID:
+var myHeading = document.getElementById( 'my-heading' );
+myHeading.title = 'This is a heading.'; // Tooltip attribute.
+// myHeading.style.color = 'darkgray'; // PLEASE avoid using inline styles... this is the CSS file's job!!!
+myHeading.className = 'gray-text'; // Use classes instead!
+
+/**
+ * Events!
+ */
+
+// We can add event-listeners to a targeted element.
+myHeading.addEventListener( 'mouseover'/* Here we say which event we are targeting. */, function ( event/* We capture infor about the event*/ ) {
+    this.className = 'salmon-text'; /* In an event, the "this" keyword representd our element (myHeading). */
+})
