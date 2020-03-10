cd113e7d68aa82044b8f05f38c0f405d37b305f2
leye20 <leyeagbo@gmail.com>
2020-03-10 10:14:03 -0600

    nested functions in scope

--- day-6.js
+++ day-6.js
@@ -22,6 +22,16 @@ function myFunction () {
         console.log( 'Accessing function scope variable in nested function: '+myFunctionScopeVariable ); // We can access variables from a PARENT function. But not the reverse.
     
         console.log( myVariable ); // Global variables / functions are accessible even all the way in here!
+    
+        // More nesting
+        function myDoubleNestedFunction () {
+            var doubleNestedVar = 'Double nested var, here!';
+            console.log( doubleNestedVar );
+        }
+
+        myDoubleNestedFunction(); //This works, same scope that the function was defined.
+    
+        // myNestedFunction(); // Can i run this here? yes, it was declared in/by a parent so we are good to go! calling a function inside of itself causes recursion --> unless you have a termination condition this causes endless looping. (my browser is crashing).
     }
 
     myNestedFunction(); // Lets runa dn try it.
